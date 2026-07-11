e62a5d75ea7c8aa9b8e85200c263bbf34952d47c
SMITHY3141 <183660732+SMITHY3141@users.noreply.github.com>
2026-07-11 14:10:32 +1000

    added ability to parse .txt files passed from the command line for initial ballistic conditions. Can set target position, projectile speed, etc. txt file gets parsed into a Conditions struct which can be passed and printed easily.

---
+++ src/main.cpp
@@ -0,0 +1,146 @@
+
+
+#include <iostream>
+#include <fstream>
+
+#define SAVE_DEFAULT "params.txt"
+#define SAVE_COMMENT "//"
+
+
+struct Conditions {
+    float speed = 0.f; ///< Projectile launch speed
+    float drag = 0.f; ///< Projectile linear drag v[n+1] = v[n] - (1-drag) * v[n]
+
+    float x = 0.f;
+    float y = 0.f;
+    float z = 0.f;
+
+    float vx = 0.f;
+    float vy = 0.f;
+    float vz = 0.f;
+
+    int max = 15; ///< max number of iterations in solver
+    float desired = 0.f; ///< desired error (will stop solver early if reached)
+
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Conditions& c) {
+    os << "CONDITIONS" << '\n'
+       << "speed: " << c.speed << '\n'
+       << "drag: "  << c.drag  << '\n'
+       << "target: (" << c.x << ", " << c.y << ", " << c.z << ")\n"
+       << "velocity: (" << c.vx << ", " << c.vy << ", " << c.vz << ")\n"
+       << "max: " << c.max << '\n'
+       << "desired error: " << c.desired;
+
+    return os; // so we can chain together std::cout << inputs
+}
+
+char* find_save(int argc, char **argv);
+Conditions parse_params(char *save_path);
+
+int main(int argc, char **argv) {
+    argc--;
+    argv++;
+
+    char* save_path = find_save(argc, argv);
+    Conditions conditions = parse_params(save_path);
+
+    std::cout << conditions << '\n';
+}
+
+// TODO parse the command args for the save location (if none given use defualt)
+char* find_save(int argc, char **argv) {
+    if (!argc) {
+        return SAVE_DEFAULT;
+    }
+    return argv[0];
+
+}
+
+
+// in-place, only splits on the first instance of //
+void strip_comment(std::string &string) {
+    size_t pos = string.find(SAVE_COMMENT);
+    if (pos == std::string::npos) {
+        return; // pos returned -1, substring was not found
+    }
+    string.erase(pos); // removes everything past the pos. erase(0,5) removes in range
+
+}
+
+
+// std::vector is dynamic .push_back(), .pop_back(), .resize()
+std::array<std::string, 2> strip_past_colon(const std::string& line) {
+    std::array<std::string, 2> delimited; // "" in each string
+    
+    size_t pos = line.find(":");
+    if (pos == std::string::npos) {
+        delimited[0] = line;
+        return delimited;
+
+    }
+    std::string key = line.substr(0, pos); // everything up until pos
+    std::string val = line.substr(pos + 1); // everything beyond pos
+    
+    delimited[0] = key;
+    delimited[1] = val;
+
+    return delimited;
+
+}
+
+
+Conditions parse_params(char *save_path) {
+    Conditions conditions;
+    
+    // signature of void (*)(const std::string&)
+    // [] is a capture clause, it reaches into the surrounding scope to pull variables in
+    // [&] is used in the lambda, it reaches into the surrounding scope so we can use them by reference
+    // [=] to capture by value, so capturing the entire scope by value would be expensive
+    std::unordered_map<std::string, std::function<void(const std::string&)>> setters = {
+        {"speed", [&](const std::string& v){conditions.speed = std::stof(v);}},
+        {"drag",  [&](const std::string& v){conditions.drag = std::stof(v);}},
+        {"x",     [&](const std::string& v){conditions.x = std::stof(v);}},
+        {"y",     [&](const std::string& v){conditions.y = std::stof(v);}},
+        {"z",     [&](const std::string& v){conditions.z = std::stof(v);}},
+        {"vx",    [&](const std::string& v){conditions.vx = std::stof(v);}},
+        {"vy",    [&](const std::string& v){conditions.vy = std::stof(v);}},
+        {"vz",    [&](const std::string& v){conditions.vz = std::stof(v);}},
+        {"max",   [&](const std::string& v){conditions.max = std::stoi(v);}},
+        {"desired", [&](const std::string& v){conditions.desired = std::stof(v);}}
+    };
+
+    //ifstream as in input fstream, there's also ofstream
+    std::ifstream file(save_path);
+
+    std::string line;
+    while (std::getline(file, line)) {
+        strip_comment(line);
+        std::array<std::string, 2> key_value = strip_past_colon(line);
+        std::string key = key_value[0];
+        std::string val = key_value[1];
+
+        if (!key.length()) {
+            continue; // if key empty (most likely line was empty)
+        }
+
+        // .find() returns an iterator
+        // say int nums[] = {1,2}; int *p = nums;
+        // iterator in cpp is it's way of abstracting this p ptr
+        // nums.begin() is an iterator, deref to get 1, it++
+        // in .find() case, the iterator is of the entire map;
+        auto it = setters.find(key); // iterator of key value pair
+        if (it != setters.end()) {
+            it->second(val);
+        }
+    }
+
+    return conditions;
+
+}
+
+
+
+
